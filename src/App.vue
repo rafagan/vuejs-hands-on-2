ba4dd656ae3811b70e7dc0766be0cd9aa20f6331
Rafagan Abreu <rafagansa@outlook.com>
2019-05-28 02:51:06 -0300

    Cria um header, reorganiza App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
@@ -33,3 +30,14 @@
     background: #666;
   }
 </style>
+
+<script>
+  import Header from '@/components/Header'
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,38 +5,38 @@
 </template>
 
 <script>
-import Todos from '@/components/Todos'
+  import Todos from '@/components/Todos'
 
-export default {
-  name: 'home',
-  components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        },
-      ]
+  export default {
+    name: 'Home',
+    components: {
+      Todos
+    },
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: 'Todo One',
+            completed: false
+          },
+          {
+            id: 2,
+            title: 'Todo Two',
+            completed: true
+          },
+          {
+            id: 3,
+            title: 'Todo Three',
+            completed: false
+          },
+        ]
+      }
+    },
+    methods: {
+      deleteTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+      }
     }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
-    }
-  }
 }
 </script>
