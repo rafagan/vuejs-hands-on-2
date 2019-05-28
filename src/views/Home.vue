f86c60f9d49ab499ba10e40bb8b5890b60b2d66d
Rafagan Abreu <rafagansa@outlook.com>
2019-05-28 03:02:48 -0300

    Instalar lib uuid, spread operator, tag form com eventos e prevent default

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,47 @@
 <template>
   <div class="home">
-    <Todos :todos="todos" @del-todo="deleteTodo" />
+      <AddTodo @add-todo="addTodo" />
+      <Todos :todos="todos" @del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-  import Todos from '@/components/Todos'
+    import Todos from '@/components/Todos'
+    import AddTodo from '@/components/AddTodo'
 
-  export default {
-    name: 'Home',
-    components: {
-      Todos
-    },
-    data() {
-      return {
-        todos: [
-          {
-            id: 1,
-            title: 'Todo One',
-            completed: false
-          },
-          {
-            id: 2,
-            title: 'Todo Two',
-            completed: true
-          },
-          {
-            id: 3,
-            title: 'Todo Three',
-            completed: false
-          },
-        ]
-      }
-    },
-    methods: {
-      deleteTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id)
-      }
+    export default {
+        name: 'Home',
+        components: {
+            Todos, AddTodo
+        },
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        title: 'Todo One',
+                        completed: false
+                    },
+                    {
+                        id: 2,
+                        title: 'Todo Two',
+                        completed: true
+                    },
+                    {
+                        id: 3,
+                        title: 'Todo Three',
+                        completed: false
+                    },
+                ]
+            }
+        },
+        methods: {
+            deleteTodo(id) {
+                this.todos = this.todos.filter(todo => todo.id !== id)
+            },
+            addTodo(newTodo) {
+                this.todos = [...this.todos, newTodo]
+            }
+        }
     }
-}
 </script>
