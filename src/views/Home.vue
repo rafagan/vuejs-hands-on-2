bf16c2a07462710a435d4d96723a24313f0ab9f6
Rafagan Abreu <rafagansa@outlook.com>
2019-05-28 03:12:56 -0300

    Mock API todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
 <script>
     import Todos from '@/components/Todos'
     import AddTodo from '@/components/AddTodo'
+    import axios from 'axios'
 
     export default {
         name: 'Home',
@@ -16,32 +17,31 @@
         },
         data() {
             return {
-                todos: [
-                    {
-                        id: 1,
-                        title: 'Todo One',
-                        completed: false
-                    },
-                    {
-                        id: 2,
-                        title: 'Todo Two',
-                        completed: true
-                    },
-                    {
-                        id: 3,
-                        title: 'Todo Three',
-                        completed: false
-                    },
-                ]
+                todos: []
             }
         },
         methods: {
             deleteTodo(id) {
-                this.todos = this.todos.filter(todo => todo.id !== id)
+                axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+                     .then(_ => this.todos = this.todos.filter(todo => todo.id !== id))
+                     .catch(err => console.log(err));
             },
             addTodo(newTodo) {
-                this.todos = [...this.todos, newTodo]
+                const { title, completed } = newTodo;
+
+                axios.post('https://jsonplaceholder.typicode.com/todos', {
+                    title,
+                    completed
+                }).then(res => res.data)
+                  .then(todo => this.todos = [...this.todos, todo])
+                  .catch(err => console.log(err));
             }
+        },
+        created() {
+            axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+                .then(res => res.data)
+                .then(todos => this.todos = todos)
+                .catch(err => console.log(err));
         }
     }
 </script>
